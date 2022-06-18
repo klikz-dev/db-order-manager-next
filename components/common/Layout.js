7e2e327403014853da516881317022beecec20b9
Murrell Johnson <dev@klikz.us>
2022-06-18 08:00:26 -0400

    product init

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,7 +1,51 @@
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import Divider from '../atoms/Divider'
+import Loading from '../atoms/Loading'
+import Footer from '../organisms/Footer'
+import Header from '../organisms/Header'
+import SideNav from '../organisms/SideNav'
+
 export default function Layout({ children }) {
+  const router = useRouter()
+  const { data: session, status } = useSession()
+
+  if (status === 'authenticated') {
+    if (session?.accessTokenExpires < new Date().getTime()) {
+      signOut()
+    } else {
+      return (
+        <>
+          <Header />
+          <main id='page-content'>
+            <div className='flex'>
+              <div id='sideNav' className='w-0 xl:w-auto'>
+                <SideNav />
+              </div>
+              <div id='dashboard' className='min-h-full w-full py-16'>
+                {children}
+              </div>
+            </div>
+          </main>
+
+          <Divider width={12} />
+          <Footer />
+        </>
+      )
+    }
+  } else if (status === 'unauthenticated') {
+    router.push('/')
+  }
+
   return (
     <>
-      <main id='page-content'>{children}</main>
+      <Header />
+      <main id='page-content'>
+        <div className='flex'>
+          <Loading />
+        </div>
+      </main>
+      <Footer />
     </>
   )
 }
