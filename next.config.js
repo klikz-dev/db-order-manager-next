7d61dfb4cd7277762de37740991f68b00b6165e4
Murrell Johnson <dev@klikz.us>
2023-04-13 16:50:58 -0400

    chore: disable next image optimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(',') },
+  images: {
+    unoptimized: true,
+    domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(','),
+  },
 }
 
 module.exports = nextConfig
